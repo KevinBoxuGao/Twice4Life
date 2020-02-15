979d5a3cb89b2f248c1454a660fc6d767d54c51d
Cyndakwil <rshetty449@gmail.com>
2020-02-15 13:55:32 -0500

    deletus fetus

--- test2.py
+++ test2.py
@@ -8,9 +8,8 @@ path = os.path.realpath("npydata/audio1.npy")
 data = np.load(path)
 song = []
 for bucket in data:
-    dft = np.fft.fft(bucket)[:4097]
-    dft2 = np.append(dft, -1*dft[-1:1:-1])
-    song.append(np.fft.ifft(dft2))
+    dft = np.fft.fft(bucket)
+    song.append(np.fft.ifft(dft))
 song = np.array(song).reshape(1, -1)
 output = torch.Tensor(np.array([i.real for i in song]))
 torchaudio.save("testfile1.wav", output, sample_rate=16000)
