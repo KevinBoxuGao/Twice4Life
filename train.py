7e7cdd8a567ccc48a9ccac36dbf0d80d1ce3b699
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-15 19:29:05 +0000

    push

--- train.py
+++ train.py
@@ -39,13 +39,13 @@ criterion = nn.MSELoss()
 min_loss = -np.inf
 for epoch in range(START_EPOCH, N_EPOCHS + 1):
     # Load data
-    for i in range(1, 5):
+    ctr = 0
+    for i in range(1, 13):
         song = torch.Tensor(np.load("npydata/audio" + str(i) + ".npy")).to(device)
         outputs = []
 
         bucket = song[0]
         bucket = np.fft.fft(bucket.cpu().numpy())
-        bucket = bucket[:4096]
         bucket = torch.Tensor([x.real for x in bucket] + [x.imag for x in bucket]).to(device).view(-1)
         for j in range(1, len(song)):
             optimizer.zero_grad()
@@ -55,28 +55,30 @@ for epoch in range(START_EPOCH, N_EPOCHS + 1):
 
             next_bucket = song[j]
             next_bucket = np.fft.fft(next_bucket.cpu().numpy())
-            next_bucket = next_bucket[:4096]
             next_bucket = torch.Tensor([x.real for x in next_bucket] + [x.imag for x in next_bucket]).to(device).view(-1)
 
             loss = criterion(output, next_bucket)
             loss.backward(retain_graph = True)
             optimizer.step()
 
+            ctr += 1
+            print("Step", ctr)
+
             bucket = next_bucket
         outputs = np.asarray(outputs)
 
         for j in range(len(outputs)):
-            outputs[j] = np.array([complex(outputs[j][k], outputs[j][4096:][k]) for k in range(4096)] + [(0+0j)]*4096)
-            outputs[j] = np.append(outputs[j][:4097], (-1*outputs[j][4095:0:-1]))
+            outputs[j] = np.array([complex(outputs[j][k], outputs[j][4096:][k]) for k in range(4096)] + [(0+0j)]*4096, dtype = np.complex128)
             outputs[j] = np.fft.ifft(outputs[j])
             for k in range(len(outputs[j])):
                 outputs[j][k] = outputs[j][k].real
         outputs = torch.Tensor(outputs.reshape(-1)).view(1, -1)
-        torchaudio.save("out/epoch" + str(epoch) + ".wav", outputs, sample_rate = 16000)
+        torchaudio.save("out/epoch" + str(epoch) + "_" + str(i) + ".wav", outputs, sample_rate = 16000)
         if loss < min_loss:
             min_loss = loss
             torch.save(model.state_dict(), "models/generator_" + MODEL_ID + ".pth")
-        print("Epoch:", epoch)
-        print("---------------")
+        print("Epoch:", epoch, "-", i)
+        print("------------------")
         print("Loss:", loss.item())
+        print("Difference (Real - Fake):", torch.mean(song.view(-1) - outputs.view(-1)))
         print()
